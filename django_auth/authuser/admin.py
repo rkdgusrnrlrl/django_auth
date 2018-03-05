02ecb5abc4891aa2e81748654943e40486a3a7ee
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2018-03-05 22:42:33 +0900

    feature:add usermanager and extends authuser for admin

--- django_auth/authuser/admin.py
+++ django_auth/authuser/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from  django import forms
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from django_auth.authuser.models import AuthUser
+
+
+class UserCreationForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password_confirm = forms.CharField(label='Passwordconfirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = AuthUser
+        fields = ('email', 'name')
+
+    def clean_password_confirm(self):
+        password = self.cleaned_data.get("password")
+        password_confirm = self.cleaned_data.get("password_confirm")
+
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError("Password dont match")
+
+        return password
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+
+        if commit:
+            user.save()
+
+        return user
+
--- django_auth/authuser/models.py
+++ django_auth/authuser/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+
+
+class AuthUserManager(BaseUserManager):
+    def create_user(self, email, name, password=None):
+        if not email:
+            raise ValueError("User must have an email")
+
+        authuser = self.model(email=email, name=name)
+        authuser.set_password(password)
+        authuser.save()
+        return authuser
+
+    def create_superuser(self, email, name, password):
+        authuser = self.model(email=email, name=name, password=password)
+        authuser.is_admin = True
+        authuser.save()
+
+class AuthUser(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=20)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = AuthUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ['name']
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def hello(self):
+        return self.name + " hello"
+
+    @property
+    def is_staff(self):
+        return self.is_admin
